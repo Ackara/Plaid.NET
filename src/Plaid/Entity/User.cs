e1aa5bf18acb886df733cd79cba3b9abe06e6e2a
Dylan Attal <dylan.attal@ozk.com>
2021-07-07 11:52:51 -0400

    Adds PhoneNumber and PhoneNumberVerifiedTime to User

--- src/Plaid/Entity/User.cs
+++ src/Plaid/Entity/User.cs
@@ -13,5 +13,19 @@ namespace Acklann.Plaid.Entity
         /// <value>The client user id.</value>
         [JsonProperty("client_user_id")]
         public string ClientUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client phone number.
+        /// </summary>
+        /// <value>The client phone number.</value>
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the verification time of the client phone number.
+        /// </summary>
+        /// <value>The verification time of the client phone number.</value>
+        [JsonProperty("phone_number_verified_time")]
+        public string PhoneNumberVerifiedTime { get; set; }
     }
 }

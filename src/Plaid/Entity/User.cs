70fbf40854a67eb77af876692a203e8ce77e82d2
Dylan Attal <41970259+DylanAttal@users.noreply.github.com>
2021-07-07 16:32:01 -0400

    Fixes indentation

--- src/Plaid/Entity/User.cs
+++ src/Plaid/Entity/User.cs
@@ -14,39 +14,39 @@ namespace Acklann.Plaid.Entity
         [JsonProperty("client_user_id")]
         public string ClientUserId { get; set; }
 
-		/// <summary>
-		/// Gets or sets the client legal name.
-		/// </summary>
-		/// <value>The client legal name.</value>
-		[JsonProperty("legal_name")]
-		public string LegalName { get; set; }
+	/// <summary>
+	/// Gets or sets the client legal name.
+	/// </summary>
+	/// <value>The client legal name.</value>
+	[JsonProperty("legal_name")]
+	public string LegalName { get; set; }
 
-		/// <summary>
-		/// Gets or sets the client phone number.
-		/// </summary>
-		/// <value>The client phone number.</value>
-		[JsonProperty("phone_number")]
-        public string PhoneNumber { get; set; }
+	/// <summary>
+	/// Gets or sets the client phone number.
+	/// </summary>
+	/// <value>The client phone number.</value>
+	[JsonProperty("phone_number")]
+	public string PhoneNumber { get; set; }
 
-        /// <summary>
-        /// Gets or sets the verification time of the client phone number.
-        /// </summary>
-        /// <value>The verification time of the client phone number.</value>
-        [JsonProperty("phone_number_verified_time")]
-        public string PhoneNumberVerifiedTime { get; set; }
+	/// <summary>
+	/// Gets or sets the verification time of the client phone number.
+	/// </summary>
+	/// <value>The verification time of the client phone number.</value>
+	[JsonProperty("phone_number_verified_time")]
+	public string PhoneNumberVerifiedTime { get; set; }
 
-		/// <summary>
-		/// Gets or sets the client email address.
-		/// </summary>
-		/// <value>The client email address.</value>
-		[JsonProperty("email_address")]
-		public string EmailAddress { get; set; }
+	/// <summary>
+	/// Gets or sets the client email address.
+	/// </summary>
+	/// <value>The client email address.</value>
+	[JsonProperty("email_address")]
+	public string EmailAddress { get; set; }
 
-		/// <summary>
-		/// Gets or sets the verification time of the client email address.
-		/// </summary>
-		/// <value>The verification time of the client email address.</value>
-		[JsonProperty("email_address_verified_time")]
-		public string EmailAddressVerifiedTime { get; set; }
-	}
+	/// <summary>
+	/// Gets or sets the verification time of the client email address.
+	/// </summary>
+	/// <value>The verification time of the client email address.</value>
+	[JsonProperty("email_address_verified_time")]
+	public string EmailAddressVerifiedTime { get; set; }
+    }
 }

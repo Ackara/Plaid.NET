7d3d1bee35f833eb99eb26a73a32515fe95a898a
Dylan Attal <dylan.attal@ozk.com>
2021-07-07 11:58:50 -0400

    updates CreateLinkTokenRequest

--- src/Plaid/Management/CreateLinkTokenRequest.cs
+++ src/Plaid/Management/CreateLinkTokenRequest.cs
@@ -106,6 +106,20 @@ namespace Acklann.Plaid.Management
 			/// <value>The client user id.</value>
 			[JsonProperty("client_user_id")]
 			public string ClientUserId { get; set; }
+
+			/// <summary>
+			/// Gets or sets the client phone number.
+			/// </summary>
+			/// <value>The client phone number.</value>
+			[JsonProperty("phone_number")]
+			public string PhoneNumber { get; set; }
+
+			/// <summary>
+			/// Gets or sets the verification time of the client phone number.
+			/// </summary>
+			/// <value>The verification time of the client phone number.</value>
+			[JsonProperty("phone_number_verified_time")]
+			public string PhoneNumberVerifiedTime { get; set; }
 		}
 	}
 }
